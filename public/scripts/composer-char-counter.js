b67e1b8e55dac875a6561efe6eba755f4185fa5f
SeanSFitz <sean.s.fitz@gmail.com>
2017-06-12 22:11:31 +0000

    Finished up to styling individual tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+const MAX_LENGTH = 140;
+
+$(document).ready(() => {
+  const inputBox = $(".new-tweet").find("form textarea");
+  inputBox.on("keyup", function () {
+    const counter = $(this).parents("form").children(".counter");
+    let value = MAX_LENGTH - $(this).val().length;
+    counter.text(value);
+    if (value < 0) {
+      counter.addClass("too-long");
+    } else {
+      counter.removeClass("too-long");
+    }
+  });
+});
